f3afce6bb60de6967fb84f3565c268884df0f79d
Ravi Punj <ravi.punj@gmail.com>
2017-11-05 17:53:08 -0500

    Added more models

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import os
+
 import datetime
 import uuid
 
@@ -22,8 +24,38 @@ class ModifiedMixin(object):
 class BaseModelMixin(UUIDMixin, CreatedMixin, ModifiedMixin):
     pass
 
+
+@attrs_sqlalchemy
+class User(BaseModelMixin, db.Model):
+    __tablename__ = "user"
+
+    username = db.Column(db.String(), nullable=False, index=True)
+    password_hash = db.Column(db.String(), nullable=False)
+    password_salt = db.Column(db.String(16), nullable=False)
+    display_name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+
+
+@attrs_sqlalchemy
+class Deck(BaseModelMixin, db.Model):
+    __tablename__ = "deck"
+
+    name = db.Column(db.String(), nullable=False)
+    owner_id = db.Column(db.String(32), db.ForeignKey("user.id"))
+
+    owner = db.relationship("User", back_populates="decks")
+
+
+User.decks = db.relationship("Deck", order_by=Deck.id, back_populates="owner")
+
+
 @attrs_sqlalchemy
 class Card(BaseModelMixin, db.Model):
     __tablename__ = "card"
 
     data = db.Column(JSON, nullable=False)
+    deck_id = db.Column(db.String(32), db.ForeignKey("deck.id"))
+    deck = db.relationship("Deck", back_populates="cards")
+
+
+Deck.cards = db.relationship("Card", order_by=Card.id, back_populates="deck")
