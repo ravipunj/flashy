704f289e22c59b2b568c86229f55d11640bf9d3a
Ravi Punj <ravi.punj@gmail.com>
2017-11-05 14:57:51 -0500

    Added Card model and database config

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     DEVELOPMENT = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
---
+++ models.py
@@ -0,0 +1,29 @@
+import datetime
+import uuid
+
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+from attrs_sqlalchemy import attrs_sqlalchemy
+
+
+class UUIDMixin(object):
+    id = db.Column(db.String(32), primary_key=True, default=lambda: uuid.uuid4().hex)
+
+
+class CreatedMixin(object):
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+
+
+class ModifiedMixin(object):
+    modified = db.Column(db.DateTime, nullable=False,
+                         default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+
+class BaseModelMixin(UUIDMixin, CreatedMixin, ModifiedMixin):
+    pass
+
+
+class Card(BaseModelMixin, db.Model):
+    __tablename__ = "card"
+
+    data = db.Column(JSON, nullable=False)
