65731465d0cb53eff292c3adf4a25b330a8a6ff1
Ravi Punj <ravi.punj@gmail.com>
2017-11-12 03:55:20 -0500

    Refactored test_user to allow reusability of code.

--- tests/test_user.py
+++ tests/test_user.py
@@ -15,7 +15,7 @@ DISPLAY_NAME = "Test User"
 EMAIL = "unit@test.com"
 
 
-def _get_sample_new_user_data(**overrides):
+def get_sample_new_user_data(**overrides):
     user_data = {
         "username": USERNAME,
         "password": PASSWORD,
@@ -27,7 +27,7 @@ def _get_sample_new_user_data(**overrides):
     return user_data
 
 
-def _get_sample_existing_user_data(**overrides):
+def get_sample_existing_user_data(**overrides):
     user_data = {
         "username": USERNAME,
         "display_name": DISPLAY_NAME,
@@ -39,12 +39,19 @@ def _get_sample_existing_user_data(**overrides):
     return user_data
 
 
+def post_user(client, data):
+    return client.post(API_URL, headers=HEADERS, data=json.dumps(data))
+
+def get_user(client, user_id):
+    return client.get("%s/%s" % (API_URL, user_id), headers=HEADERS)
+
+
 class TestUserBase(BaseTestCase):
     def post_user(self, data):
-        return self.test_client.post(API_URL, headers=HEADERS, data=json.dumps(data))
+        return post_user(self.test_client, data)
 
     def get_user(self, user_id):
-        return self.test_client.get("%s/%s" % (API_URL, user_id), headers=HEADERS)
+        return get_user(self.test_client, user_id)
 
     def get_all_users(self):
         return self.test_client.get(API_URL, headers=HEADERS)
@@ -53,21 +60,21 @@ class TestUserBase(BaseTestCase):
 class TestPostUser(TestUserBase):
     @mock.patch.object(mixins, "generate_uuid4_string", return_value="stub_id")
     def test_creates_new_user(self, _):
-        user_data = _get_sample_new_user_data()
+        user_data = get_sample_new_user_data()
 
         response = self.post_user(data=user_data)
 
         self.assertEqual(201, response.status_code)
         self.assertEqual("application/json", response.headers["Content-Type"])
 
-        expected_data = _get_sample_existing_user_data(id="stub_id")
+        expected_data = get_sample_existing_user_data(id="stub_id")
         self.assertEqual(expected_data, json.loads(response.data))
 
 
 class TestGetUser(TestUserBase):
 
     def _add_sample_user(self, **overrides):
-        user_data = _get_sample_new_user_data(**overrides)
+        user_data = get_sample_new_user_data(**overrides)
 
         response = self.post_user(user_data)
 
@@ -79,13 +86,13 @@ class TestGetUser(TestUserBase):
         response = self.get_user(user_id)
 
         self.assertEqual(200, response.status_code)
-        self.assertEqual(_get_sample_existing_user_data(id=user_id), json.loads(response.data))
+        self.assertEqual(get_sample_existing_user_data(id=user_id), json.loads(response.data))
 
-    def test_get_all_users_returns_all_users(self):
-        user1 = _get_sample_new_user_data()
-        user2 = _get_sample_new_user_data(username="tu2",
-                                          display_name="Test User #2",
-                                          email="tu2@gmail.com")
+    def test_gets_all_users(self):
+        user1 = get_sample_new_user_data()
+        user2 = get_sample_new_user_data(username="tu2",
+                                         display_name="Test User #2",
+                                         email="tu2@gmail.com")
         user1["id"] = self._add_sample_user(**user1)
         user2["id"] = self._add_sample_user(**user2)
 
@@ -94,8 +101,8 @@ class TestGetUser(TestUserBase):
         response_data["objects"].sort(key=lambda o: o["id"])
 
         user1.pop("password"), user2.pop("password")
-        expected_user1 = _get_sample_existing_user_data(**user1)
-        expected_user2 = _get_sample_existing_user_data(**user2)
+        expected_user1 = get_sample_existing_user_data(**user1)
+        expected_user2 = get_sample_existing_user_data(**user2)
         self.assert200(response)
         self.assertDictEqual(
             {
